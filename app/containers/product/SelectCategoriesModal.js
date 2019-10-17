e3ac538e2d48de9e46883f1c06245bd3e17b80d4
fredkeem <fred@zikto.com>
2019-10-17 10:29:39 +0900

    home request user info

---
+++ app/containers/product/SelectCategoriesModal.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {View, TouchableOpacity, Text, Image} from 'react-native';
+import Modal from 'react-native-modal';
+
+export default class SelectCategories extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Modal>
+        <Text>asd</Text>
+      </Modal>
+    );
+  }
+}
